cbe19c241eb1896e88615dddbaa452696d3f811b
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-08 15:00:39 -0700

    add toot.cat to the blacklist

--- Autofollow.py
+++ Autofollow.py
@@ -8,7 +8,7 @@ INSTANCE='https://mastodon.host'
 
 BLACKLIST = {
         'users':['b@icosahedron.website'],
-        'instances':['icosahedron.website','slime.global']
+        'instances':['icosahedron.website','slime.global','toot.cat']
         }
 
 # Register app - only once!
